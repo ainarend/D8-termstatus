03269b103b819310d3dd480d46a6e2bf051dccb0
Ain Arend <ainarend@gmail.com>
2017-12-20 23:07:18 +0200

    Started with the permissions.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# D8-termstatus
+# Taxonomy Term Status
+This module adds a status-flag to taxonomy terms. Using this flag it is
+possible to specify whether terms should be published or not. Users with the
+appropriate permission may access unpublished terms.
+
+## Installation
+1. Download the module
+2. Enable the module. In the admin UI, it is listed under Taxonomy Term Status package.
+3. Set appropriate permissions.
+
+## To do
+* Views integration
---
+++ src/TermStatusPermissions.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\termstatus;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class TermStatusPermissions implements ContainerInjectionInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Entity manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Construct the object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   Entity manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('entity_type.manager'));
+  }
+
+  /**
+   * Generates the user permissions for Term Status.
+   *
+   * @return array
+   */
+  public function permissions() {
+    $permissions = [];
+
+    $vocabularies = $this->entityTypeManager
+                         ->getStorage('taxonomy_vocabulary')
+                         ->loadMultiple();
+
+    if ($vocabularies) {
+      foreach ($vocabularies as $vocabulary) {
+        $permissions += [
+          'view published terms in ' . $vocabulary->id() => [
+            'title' => $this->t('View published terms in %vocabulary',
+              ['%vocabulary' => $vocabulary->label()]
+            ),
+          ],
+          'view unpublished terms in ' . $vocabulary->id() => [
+            'title' => $this->t('View unpublished terms in %vocabulary',
+              ['%vocabulary' => $vocabulary->label()]
+            ),
+          ],
+        ];
+      }
+    }
+
+    return $permissions;
+  }
+
+}
